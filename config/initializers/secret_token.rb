fa0ca962a2d860b7712dec55fd3a503728a7fd06
Nathaniel Escribano <NathanielEscribano@gmail.com>
2010-07-06 20:21:59 -0400

    BloogMe's initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '35db41325fa154b3a857ae73f9179670dcf1da5276d65b1f8c1b4aa745eef75486e98a7e84cf5bc412aa6d95d6716e74f9109387ead290df32e26eba54e6b0bd'
